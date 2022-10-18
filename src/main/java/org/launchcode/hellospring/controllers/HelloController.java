e1b0e8a18adf818b4417149dccec38a4cfb2be8a
rruiz88 <roman.ruiz816@gmail.com>
2022-10-18 17:24:35 -0500

    Adding Thymeleaf

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,83 +1,60 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
-    //path/hello
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, Spring!";
-//    }
-
-@RequestMapping(value = "hello", method = RequestMethod.POST)
-@ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
-        return createMessage(name, language);
-    }
-    ///hello/goodbye
-    @GetMapping("goodbye")
-
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
 
     // request /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-
-    public String helloParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloParam(@RequestParam String name, Model model) {
+        String localGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", localGreeting);
+        return "hello";
     }
 
     ///hello/LaunchCode
-
-    @GetMapping("{name}")
-
-    public String helloPath(@PathVariable String name) {
-        return "Goodbye, " + name + "!";
+    @GetMapping("hello{name}")
+    public String helloPath(@PathVariable String name, Model model) {
+        String localGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", localGreeting);
+        return "hello";
     }
 
     ///hello/form
     @GetMapping("form")
-
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +
-                "Enter your name: <input type='text' name='name'>\n" +
-                "<select name='language'> <option value=''>" + "--Please choose a language--</option>" +
-                "<option value='french'>French</option>\n" +
-                "<option value='english'>English</option>\n" +
-                "<option value='spanish'>Spanish</option>\n" +
-                "<option value='japanese'>Japanese</option>\n" +
-                "<option value='german'>German</option>\n" +
-                "</select>\n" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
-    public static String createMessage(String name, String language) {
-        String greeting = "";
-        if (language.equals("french")) {
-            greeting = "Bonjour";
-        } else if (language.equals("english")) {
-            greeting = "Hello";
-        } else if (language.equals("spanish")) {
-            greeting = "Hola";
-        } else if (language.equals("german")) {
-            greeting = "Hallo";
-        } else if (language.equals("japanese")) {
-            greeting = "Kon'nichiwa";
-        }
-        return greeting + " " + name;
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("Java");
+        names.add("JV");
+        names.add("LaunchCode");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
+
+//    public static String createMessage(String name, String language) {
+//        String greeting = "";
+//        if (language.equals("french")) {
+//            greeting = "Bonjour";
+//        } else if (language.equals("english")) {
+//            greeting = "Hello";
+//        } else if (language.equals("spanish")) {
+//            greeting = "Hola";
+//        } else if (language.equals("german")) {
+//            greeting = "Hallo";
+//        } else if (language.equals("japanese")) {
+//            greeting = "Kon'nichiwa";
+//        }
+//        return greeting + " " + name;
+//    }
 }
